4740271edc6dbc558d3c3bbd3f55da6257ba4892
ArnoldSucasaireTorres <64154905+ArnoldSucasaireTorres@users.noreply.github.com>
2021-12-02 09:53:24 -0500

    Delete SistemaTutoriales/SistemaTutoriales directory

--- SistemaTutoriales/SistemaTutoriales/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def hola(request):
-    return HttpResponse("Hola mundo")
-
-# Funciones de Internacionalizacion
-def index(request):
-    return render(request, "index.html",{})
